558653f74af4d363f49b903897b0c1da512a0c23
hype-armor <gamm90@gmail.com>
2015-11-27 11:38:08 -0600

    Add example.

---
+++ PluginContracts/IPlugin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PluginContracts
+{
+    public interface IPlugin
+    {
+        string Name { get; }
+        string Go(string input);
+        List<string> Actions { get; }
+    }
+}
